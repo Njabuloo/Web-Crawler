41120494de04f55129716492bdb29a30c84018f7
Njabulo Masilela <masilelanjabulo01@gmail.com>
2023-02-01 22:19:05 +0200

    normalizeURL method implementation

--- crawl.js
+++ crawl.js
@@ -1,4 +1,11 @@
-function normalizeURL(url) {}
+const normalizeURL = (url) => {
+	if (url[url.length - 1] === '/') {
+		url = url.slice(0, -1);
+	}
+	const urlObj = new URL(url);
+	const normalizedUrl = `${urlObj.host}${urlObj.pathname}`.toLowerCase();
+	return normalizedUrl;
+};
 
 module.exports = {
 	normalizeURL,
--- tests/craw.test.js
+++ tests/craw.test.js
@@ -1,22 +1,22 @@
 const { describe, test, expect } = require('@jest/globals');
-const { normalizedURL } = require('../crawl');
+const { normalizeURL } = require('../crawl.js');
 
 describe('crawl module', () => {
 	test('only return the section after the //', () => {
 		const url = 'http://wagslane.dev/path';
 		const expectedUrl = 'wagslane.dev/path';
-		expect(normalizedURL(url)).to.equal(expectedUrl);
+		expect(normalizeURL(url)).toBe(expectedUrl);
 	});
 
 	test('removes the / if it is follwed by nothing', () => {
 		const url = 'https://wagslane.dev/path/';
 		const expectedUrl = 'wagslane.dev/path';
-		expect(normalizedURL(url)).to.equal(expectedUrl);
+		expect(normalizeURL(url)).toBe(expectedUrl);
 	});
 
 	test('all letters are in small caps', () => {
 		const url = 'https://wagsLane.Dev/path';
 		const expectedUrl = 'wagslane.dev/path';
-		expect(normalizedURL(url)).to.equal(expectedUrl);
+		expect(normalizeURL(url)).toBe(expectedUrl);
 	});
 });
